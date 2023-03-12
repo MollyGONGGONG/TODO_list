aaf9d2471deb78785b3a24dc47915b6a73f71edb
Xue Gong <xuegong0031@gmail.com>
2023-03-12 15:13:38 -0700

    Adjusted Arrow button position for two lists

--- index.js
+++ index.js
@@ -115,16 +115,21 @@ const View = (() => {
     let todosCompleteTemplate = "";
 
     todos.forEach((todo) => {
-      let listTemplate = `<li><span>${todo.content}</span>
-    <button class="delete-btn" id="${todo.id}">
-    <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
-    </button>
+      let todoPending = `<li><span>${todo.content}</span>
+      <button class="delete-btn" id="${todo.id}">
+      <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+      </button>
+  
+      <button class="edit-btn" id="${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24"  aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button>
+      <button class="move-btn-right" id="${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg></button></li>`;
 
-    <button class="edit-btn" id="${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24"  aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button>
-    <button class="move-btn" id="${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg></button></li>`;
-
-      let todoPending = listTemplate;
-      let todoComplete = listTemplate;
+      let todoComplete = `<li><span>${todo.content}</span>
+      <button class="delete-btn" id="${todo.id}">
+      <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+      </button>
+  
+      <button class="edit-btn" id="${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24"  aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button>
+      <button class="move-btn-left" id="${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path></svg></button></li>`;
 
       !todo.isCompleted
         ? (todosPendingTemplate += todoPending)
@@ -219,7 +224,7 @@ const Controller = ((view, model) => {
 
   const handleMove = () => {
     function moveTodo(event) {
-      if (event.target.className === "move-btn") {
+      if (event.target.className === "move-btn-right" || "move-btn-left") {
         const id = event.target.id;
         const todo = state.todos.find((todo) => todo.id === Number(id));
         todo.isCompleted = !todo.isCompleted; //isCompleted true or false: toggled
